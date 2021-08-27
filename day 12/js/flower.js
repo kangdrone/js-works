b1d9f01b0b3e111aff442c372769e0c7380bdf63
email <tlsqja1120@gmail.com>
2021-08-27 13:20:19 +0900

    day 13 추가파일

---
+++ day 12/js/flower.js
@@ -0,0 +1,15 @@
+function showtext(){
+    //설명글 보이기
+    document.getElementById("desc").style.display = "block";
+
+    //보기버튼 숨기기
+    document.getElementById("open").style.display = "none";
+}
+
+function hidetext(){
+    //설명글 숨기기
+    document.getElementById("desc").style.display = "none";
+
+    //보기버튼 숨기기
+    document.getElementById("open").style.display = "block";
+}
